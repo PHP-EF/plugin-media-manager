fe1fc90694d70081c29e2d16797487dae17de4b7
TehMuffinMoo <mat.cox@tmmn.uk>
2024-12-31 17:45:48 +0000

    Updates to give proper examples of API usage and include a tautulli wrapper

--- api.php
+++ api.php
@@ -14,7 +14,34 @@ $app->get('/plugin/plextvcleaner/settings', function($request, $response, $args)
 		->withStatus($GLOBALS['responseCode']);
 });
 
-//To be updated
+$app->get('/plugin/plextvcleaner/tautulli/libraries', function($request, $response, $args) {
+    $plextvcleaner = new plextvcleaner();
+    if ($plextvcleaner->auth->checkAccess($plextvcleaner->config->get("Plugins", "Plex TV Cleaner")['ACL-PLEXTVCLEANER'] ?? "ACL-PLEXTVCLEANER")) {
+        $plextvcleaner->getTautulliLibraries();
+    }
+    $response->getBody()->write(jsonE($GLOBALS['api']));
+    return $response
+        ->withHeader('Content-Type', 'application/json;charset=UTF-8')
+        ->withStatus($GLOBALS['responseCode']);
+});
+
+$app->get('/plugin/plextvcleaner/tautulli/libraries/{id}', function($request, $response, $args) {
+    $plextvcleaner = new plextvcleaner();
+    if ($plextvcleaner->auth->checkAccess($plextvcleaner->config->get("Plugins", "Plex TV Cleaner")['ACL-PLEXTVCLEANER'] ?? "ACL-PLEXTVCLEANER")) {
+        $data = $request->getQueryParams();
+        $Length = $data['limit'] ?? 100;
+        $Start = ($data['page'] == 1) ? 0 : ($data['page'] * $Length ?? 0);
+        $plextvcleaner->getTautulliMediaFromLibrary($args['id'],$Start,$Length);
+    }
+    $response->getBody()->write(jsonE($GLOBALS['api']));
+    return $response
+        ->withHeader('Content-Type', 'application/json;charset=UTF-8')
+        ->withStatus($GLOBALS['responseCode']);
+});
+
+
+// ** OLD STUFF ** //
+
 $app->get('/plugin/plextvcleaner/shows', function($request, $response, $args) {
     $plextvcleaner = new plextvcleaner();
     if ($plextvcleaner->auth->checkAccess($plextvcleaner->config->get("Plugins", "Plex TV Cleaner")['ACL-PLEXTVCLEANER'] ?? "ACL-PLEXTVCLEANER")) {
