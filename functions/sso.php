eb852f5db63d34b83e07f068cc46c5049afd6d65
TehMuffinMoo <mat.cox@tmmn.uk>
2025-01-07 10:27:20 +0000

    overseerr sso

--- functions/sso.php
+++ functions/sso.php
@@ -46,7 +46,7 @@ trait SSO {
         );
         $Results = $this->api->query->post($Url,$DataArr,null,null,true);
         if (isset($Results->success)) {
-            setcookie('connect.sid', $Results->cookies['connect.sid']->value, time() + (86400 * 30), "/", ".tmmn.local"); // 30 days
+            setcookie('connect.sid', urldecode($Results->cookies['connect.sid']->value), time() + (86400 * 30), "/", ".tmmn.local"); // 30 days
             return true;
         } else {
             return false;
