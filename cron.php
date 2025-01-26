ec63f1157d0d852f73788d65c342de17cef97517
TehMuffinMoo <mat.cox@tmmn.uk>
2025-01-26 18:31:09 +0000

    Add cronjob tracking

--- cron.php
+++ cron.php
@@ -3,20 +3,30 @@
 $SonarrAndTautulliSyncronisationSchedule = $phpef->config->get('Plugins','Media Manager')['sonarrAndTautulliSyncronisationSchedule'] ?? '*/60 * * * *';
 $RadarrAndTautulliSyncronisationSchedule = $phpef->config->get('Plugins','Media Manager')['radarrAndTautulliSyncronisationSchedule'] ?? '*/60 * * * *';
 
-// Scheduled Syncronisation of Sonarr / Tautulli into tvshows Table
+// Scheduled Synchronisation of Sonarr / Tautulli into tvshows Table
 $scheduler->call(function() {
     $MediaManager = new MediaManager();
-    $pluginConfig = $MediaManager->config->get('Plugins','Media Manager');
+    $pluginConfig = $MediaManager->config->get('Plugins', 'Media Manager');
     if (isset($pluginConfig['sonarrUrl']) && isset($pluginConfig['sonarrApiKey']) && isset($pluginConfig['tautulliUrl']) && isset($pluginConfig['tautulliApiKey'])) {
-        $MediaManager->updateTVShowTable();
+        try {
+            $MediaManager->updateTVShowTable();
+            $MediaManager->updateCronStatus('Media Manager','TV Show Sync', 'success');
+        } catch (Exception $e) {
+            $MediaManager->updateCronStatus('Media Manager','TV Show Sync', 'error', $e->getMessage());
+        }
     }
 })->at($SonarrAndTautulliSyncronisationSchedule);
 
-// Scheduled Syncronisation of Radarr / Tautulli into movies Table
+// Scheduled Synchronisation of Radarr / Tautulli into movies Table
 $scheduler->call(function() {
     $MediaManager = new MediaManager();
-    $pluginConfig = $MediaManager->config->get('Plugins','Media Manager');
+    $pluginConfig = $MediaManager->config->get('Plugins', 'Media Manager');
     if (isset($pluginConfig['radarrUrl']) && isset($pluginConfig['radarrApiKey']) && isset($pluginConfig['tautulliUrl']) && isset($pluginConfig['tautulliApiKey'])) {
-        $MediaManager->updateMoviesTable();
+        try {
+            $MediaManager->updateMoviesTable();
+            $MediaManager->updateCronStatus('Media Manager','Movie Sync', 'success');
+        } catch (Exception $e) {
+            $MediaManager->updateCronStatus('Media Manager','Movie Sync', 'error', $e->getMessage());
+        }
     }
 })->at($RadarrAndTautulliSyncronisationSchedule);
